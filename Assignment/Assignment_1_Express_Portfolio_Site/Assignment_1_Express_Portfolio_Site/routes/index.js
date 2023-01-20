67c477793854cfbf28931a408d621d1675e70297
Oswald Kam <oswaldkam@gmail.com>
2023-01-20 01:22:22 -0500

    wip

--- Assignment/Assignment_1_Express_Portfolio_Site/Assignment_1_Express_Portfolio_Site/routes/index.js
+++ Assignment/Assignment_1_Express_Portfolio_Site/Assignment_1_Express_Portfolio_Site/routes/index.js
@@ -1,22 +1,26 @@
 var express = require("express");
 var router = express.Router();
+var projectData = require("../data/projectData.js");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("basePage", { title: "Home", page: "home" });
+  res.render("basePage", { title: "Home", page: "home", data: null });
 });
 router.get("/aboutMe", function (req, res, next) {
-  res.render("basePage", { title: "About Me", page: "aboutMe" });
+  res.render("basePage", { title: "About Me", page: "aboutMe", data: null });
 });
 router.get("/services", function (req, res, next) {
-  res.render("basePage", { title: "Services", page: "services" });
+  res.render("basePage", { title: "Services", page: "services", data: null });
 });
 router.get("/projects", function (req, res, next) {
-  res.render("basePage", { title: "Project", page: "projects" });
+  res.render("basePage", {
+    title: "Project",
+    page: "projects",
+    data: projectData.projectData,
+  });
 });
 router.get("/contact", function (req, res, next) {
-  res.render("basePage", { title: "Contact", page: "contact" });
+  res.render("basePage", { title: "Contact", page: "contact", data: null });
 });
 
 module.exports = router;
-
